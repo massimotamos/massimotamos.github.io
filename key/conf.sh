43900ee73ef17f4e3bbf7df927c7901dcf6af07e
Kaan S. Karadag <KaanSK@users.noreply.github.com>
2020-02-26 00:22:49 +0200

    Update conf.sh

--- key/conf.sh
+++ key/conf.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y wget gnupg2 gnupg-agent scdaemon pcscd git 
 cd ~/.gnupg
-wget https://raw.githubusercontent.com/drduh/config/master/gpg-agent.conf
+wget https://raw.githubusercontent.com/KaanSK/kaansk.github.io/master/key/gpg-agent.conf
 echo "export GPG_TTY=\"\$(tty)\"" >> ~/.bashrc
 echo "export SSH_AUTH_SOCK=\"/run/user/$UID/gnupg/S.gpg-agent.ssh\"" >> ~/.bashrc
 echo "gpg-connect-agent updatestartuptty /bye > /dev/null" >> ~/.bashrc
